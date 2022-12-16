453fcf38ab543821faea3ba3abb7f7a9b2f6b9d5
avanim <axm1205@case.edu>
2022-12-16 17:11:20 -0500

    added option to view total budget of dept, ordered employee table by ID

--- index.js
+++ index.js
@@ -34,6 +34,45 @@ async function getDeptList() {
     }
 }
 
+// View table of combined salaries of all employees in certain department
+async function viewTotalBudget() {
+    const deptChoices = await getDeptList();
+    const deptNames = [];
+    deptChoices.forEach(dept => deptNames.push(dept.name));
+
+    const viewBudgetDeptQ = [
+        {
+            type: 'list',
+            message: 'For which department would you like to see the total utilized budget?',
+            choices: deptNames,
+            name: 'deptBudget'
+        }
+    ];
+
+    inquirer
+        .prompt(viewBudgetDeptQ)
+        .then(async (data) => {
+            try {
+                const result = await db.execute(`SELECT department.name as 'DEPARTMENT', SUM(role.salary) AS 'TOTAL BUDGET'
+                                                FROM employee AS emp1 LEFT JOIN employee AS emp2 ON emp1.manager_id = emp2.id JOIN role ON emp1.role_id = role.id 
+                                                JOIN department ON role.department_id = department.id WHERE department.name = ? GROUP BY role.department_id`, [data.deptBudget]);
+        
+                // If budget table empty, inform user, otherwise display table
+                if (result[0].length === 0) {
+                    console.log('No employees have been added to this department yet.\n');
+                    const value = [[data.deptBudget, 0]];
+                    console.table(['DEPARTMENT', 'TOTAL BUDGET'], value);
+                } else {
+                    // console.log('\n');
+                    console.table(result[0]);
+                }
+                showMenu();
+            } catch (err) {
+                console.error(err);
+            }
+        });
+}
+
 async function updateEmployee() {
     const roleChoices = await getRoleList();
     const roleTitles = [];
@@ -254,7 +293,7 @@ async function viewEmployees() {
         const result = await db.execute(`SELECT emp1.id AS 'ID', emp1.first_name AS 'FIRST NAME', emp1.last_name AS 'LAST NAME', 
                                     role.title AS 'TITLE', department.name AS 'DEPARTMENT', salary AS 'SALARY', IF(ISNULL(emp1.manager_id), 'null', CONCAT_WS(' ', emp2.first_name, emp2.last_name)) AS 'MANAGER' 
                                     FROM employee AS emp1 LEFT JOIN employee AS emp2 ON emp1.manager_id = emp2.id JOIN role ON emp1.role_id = role.id 
-                                    JOIN department ON role.department_id = department.id`);
+                                    JOIN department ON role.department_id = department.id ORDER BY ID`);
 
         // If employee table empty, inform user, otherwise display table
         if (result[0].length === 0) {
@@ -307,7 +346,7 @@ function showMenu() {
         {
             type: 'list',
             message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'View total utilized budget of a department', 'Quit'],
             name: 'action'
         }
     ];
@@ -329,6 +368,8 @@ function showMenu() {
                 addEmployee();
             } else if (menuA.action === 'Update an employee role') {
                 updateEmployee();
+            } else if (menuA.action === 'View total utilized budget of a department') {
+                viewTotalBudget();
             } else if (menuA.action === 'Quit') {
                 console.log('Goodbye!');  
                 process.exit();    
